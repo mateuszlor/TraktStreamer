7181343c4cfaa8f24974acc40c8833713feae985
Mateusz Lorencki <mateuszlorencki@gmail.com>
2017-06-17 20:26:34 +0200

    base AOP logger interceptor

---
+++ TraktStreamer.AOP/ServiceExecutionInterceptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AopAlliance.Intercept;
+
+namespace TraktStreamer.AOP
+{
+    public class ServiceExecutionInterceptor : IMethodInterceptor
+    {
+        public object Invoke(IMethodInvocation invocation)
+        {
+            var sw = Stopwatch.StartNew();
+            var retValue = invocation.Proceed();
+            sw.Stop();
+
+            Debug.WriteLine("### Execution of {0}.{1}() took {2}ms ({3})", invocation.Method.DeclaringType?.Assembly.GetName().Name, invocation.Method.Name , sw.ElapsedMilliseconds, sw.Elapsed);
+
+            return retValue;
+        }
+    }
+}
