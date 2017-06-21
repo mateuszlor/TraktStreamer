8bc30720c3ddd5a72c75db4963c038c9e5a00e59
Mateusz Lorencki <mateuszlorencki@gmail.com>
2017-06-22 00:13:42 +0200

    Torrent resolution enum custom ToString()

--- TraktStreamer.Model/Enum/TorrentResolutionEnum.cs
+++ TraktStreamer.Model/Enum/TorrentResolutionEnum.cs
@@ -8,4 +8,20 @@ namespace TraktStreamer.Model.Enum
         _720p,
         _1080p
     }
+
+    public static class TorrentResolutionEnumHelper
+    {
+        public static string ToString(this TorrentResolutionEnum value)
+        {
+            switch (value)
+            {
+                case TorrentResolutionEnum._1080p:
+                    return "1080p";
+                case TorrentResolutionEnum._720p:
+                    return "720p";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
 }
