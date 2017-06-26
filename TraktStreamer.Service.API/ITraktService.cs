e96c16fd187d881235dc63332177e1f3e1edec4a
Mateusz Lorencki <mateuszlorencki@gmail.com>
2017-06-26 15:17:26 +0200

    Getting epizodes to watch

--- TraktStreamer.Service.API/ITraktService.cs
+++ TraktStreamer.Service.API/ITraktService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TraktApiSharp;
+using TraktStreamer.Model;
 
 namespace TraktStreamer.Service.API
 {
@@ -12,5 +14,7 @@ namespace TraktStreamer.Service.API
         /// <param name="userInputCallback">User inpur authorization callback (takes URL to Trakt.TV, returns PIN)</param>
         /// <returns></returns>
         Task<TraktClient> GetAuthorizedTraktClientAsync(Func<string, string> userInputCallback);
+
+        Task<ICollection<EpizodeModel>> GetAllEpizodesToWatchAsync(TraktClient client);
     }
 }
--- TraktStreamer.Service/TraktService.cs
+++ TraktStreamer.Service/TraktService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using NLog;
 using TraktApiSharp;
 using TraktApiSharp.Authentication;
 using TraktStreamer.DAO.Model;
+using TraktStreamer.Model;
 using TraktStreamer.Repository.API;
 using TraktStreamer.Service.API;
 
@@ -17,7 +20,7 @@ namespace TraktStreamer.Service
         private IAuthorizationInfoRepository AuthorizationInfoRepository;
 
         private Logger _logger = LogManager.GetCurrentClassLogger();
-
+        
         public async Task<TraktClient> GetAuthorizedTraktClientAsync(Func<string, string> userInputCallback)
         {
             var client = new TraktClient(CLIENT_ID, CLIENT_SECRET);
@@ -53,6 +56,40 @@ namespace TraktStreamer.Service
             _logger.Debug("AccessToken: {0}, RefreshToken: {1}", client.Authorization.AccessToken, client.Authorization.RefreshToken);
 
             return client;
+        }
+
+        public async Task<ICollection<EpizodeModel>> GetAllEpizodesToWatchAsync(TraktClient client)
+        {
+            var watchlist = await client.Sync.GetWatchedShowsAsync();
+            var toWatch = new List<EpizodeModel>();
+
+            _logger.Debug("Got {0} series in watchlist", watchlist.Count());
+
+            foreach (var show in watchlist)
+            {
+                var progress = await client.Shows.GetShowWatchedProgressAsync(show.Show.Ids.Slug);
+
+                if (progress.Aired > progress.Completed)
+                {
+                    toWatch.Add(new EpizodeModel
+                    {
+                        Series = new SeriesModel
+                        {
+                            TraktSlug = show.Show.Ids.Slug,
+                            Name = show.Show.Title
+                        },
+                        Season = progress.NextEpisode.SeasonNumber.GetValueOrDefault(),
+                        Epizode = progress.NextEpisode.Number.GetValueOrDefault()
+                    });
+                }
+            }
+
+            // TODO sync with database
+            // var series = toWatch.Select(e => e.Series.TraktSlug).Distinct();
+
+            _logger.Debug("Got {0} epizodes to watch", toWatch.Count);
+
+            return toWatch;
         }
     }
 }
