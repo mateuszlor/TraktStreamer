4bdebc313e995fa133c38767b8c2c0ea5dbb81c1
Mateusz Lorencki <mateuszlorencki@gmail.com>
2017-06-26 15:17:06 +0200

    Series & Epizode in applicatiom nodel

---
+++ TraktStreamer.Model/EpizodeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraktStreamer.Model
+{
+    public class EpizodeModel:BaseApplicationModel
+    {
+        public SeriesModel Series { get; set; }
+        public int Season { get; set; }
+        public int Epizode { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Series.Name} S{Season:00}E{Epizode:00}";
+        }
+    }
+}
---
+++ TraktStreamer.Model/SeriesModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraktStreamer.Model
+{
+    public class SeriesModel : BaseApplicationModel
+    {
+        public string Name { get; set; }
+        public string TraktSlug { get; set; }
+        public bool ToDownload { get; set; }
+        public bool HasCustomLimits { get; set; }
+        public long? Limit1080 { get; set; }
+        public long? Limit720 { get; set; }
+    }
+}
